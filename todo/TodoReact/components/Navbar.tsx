cd85f70aee858466805b44e7934a6d2a4a3da0c4
AmitYasharr <117912846+AmitYasharr@users.noreply.github.com>
2023-08-17 19:55:24 +0000

    5

--- todo/TodoReact/components/Navbar.tsx
+++ todo/TodoReact/components/Navbar.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import '../styles/Navbar.css'
 
-const Navbar = () =>{
+type propsType = {
+    changeMode:any;
+}
+
+const Navbar = (props:propsType) =>{
 
     const [mode,setMode] = useState<Number | null>(0);
     
     return(
         <nav>
             <ul className='list-titles'>
-                <li className={`all${mode==0? " selcted":""}`} onClick={()=>setMode(0)}>All</li>
-                <li className={`uncomplite${mode==1? " selcted":""}`} onClick={()=>setMode(1)}>Uncomplite</li>
-                <li className={`complited${mode==2? " selcted":""}`} onClick={()=>setMode(2)}>Complited</li>
+                <li className={`all${mode==0? " selcted":""}`} onClick={()=>{setMode(0);props.changeMode(0)}}>All</li>
+                <li className={`uncomplite${mode==1? " selcted":""}`} onClick={()=>{setMode(1);props.changeMode(1)}}>Uncomplite</li>
+                <li className={`complited${mode==2? " selcted":""}`} onClick={()=>{setMode(2);props.changeMode(2)}}>Complited</li>
             </ul>
         </nav>
     )
--- todo/TodoReact/src/App.tsx
+++ todo/TodoReact/src/App.tsx
@@ -2,24 +2,40 @@ import NavBar from '../components/Navbar';
 import AddTasksArea from '../components/AddTaksArea'
 import TaskArea from '../components/TaskArea'
 import '../styles/App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   const [tasks,setTasks] = useState([{id:0,taskData:"Amit",taskStatus:0}]);
+  const [filterdTasks,setFilterdTasks] = useState(tasks);
   const [mode,setMode] = useState(0);
 
+  useEffect(()=>{
+    if(mode==0){setFilterdTasks(tasks);}
+    if(mode==1){
+      const oldTasks = [...tasks];
+      const newTasks = oldTasks.filter((task)=>{return(task.taskStatus<1)});
+      setFilterdTasks(newTasks);
+    }
+    if(mode==2){
+      const oldTasks = [...tasks];
+      const newTasks = oldTasks.filter((task)=>{return task.taskStatus>0});
+      setFilterdTasks(newTasks);
+    }
+  },[mode])
+
   const changeTask = (taskID:number) =>{
-    const updateTasks = [...tasks];
+    const updateTasks = [...filterdTasks];
     if(updateTasks[taskID].taskStatus == 1){updateTasks[taskID].taskStatus=0}
     else if(updateTasks[taskID].taskStatus == 0){updateTasks[taskID].taskStatus=1}
-    setTasks(updateTasks);
+    setFilterdTasks(updateTasks);
   }
 
   const addTask = (data:string) => {
     const updateTasks = [...tasks];
     updateTasks.push({id:updateTasks[updateTasks.length-1].id+1,taskData:data,taskStatus:0})
     setTasks(updateTasks);
+    setFilterdTasks(updateTasks);
   }
 
   const changeMode = (aMode:number) =>{
@@ -30,8 +46,8 @@ function App() {
     <>
       <h1>Todo</h1>
       <AddTasksArea addTask={addTask}></AddTasksArea>
-      <NavBar/>
-      <TaskArea tasks={tasks} changeTask={changeTask}></TaskArea>
+      <NavBar changeMode={changeMode}></NavBar>
+      <TaskArea tasks={filterdTasks} changeTask={changeTask}></TaskArea>
     </>
   )
 }
